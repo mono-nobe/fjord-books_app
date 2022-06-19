a7154c7f7af38403ca282144062bd1c3e7361974
mononobe <ryoya.goemono@gmail.com>
2022-06-19 23:18:57 +0900

    新規カラムの保存処理まで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:post_code,:address,:self_introduction])
+  end
 end
