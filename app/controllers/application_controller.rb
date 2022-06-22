65136ac8e1611894d07f0abeecb14a32810e43bc
mononobe <ryoya.goemono@gmail.com>
2022-06-22 23:20:33 +0900

    アカウント関連ページの相互連携まで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters_to_sign_up, if: :devise_controller?
+  before_action :configure_permitted_parameters_to_update_account, if: :devise_controller?
 
-  def configure_permitted_parameters
+  def configure_permitted_parameters_to_sign_up
     devise_parameter_sanitizer.permit(:sign_up,keys: [:post_code,:address,:self_introduction])
+  end
+
+  def configure_permitted_parameters_to_update_account
+    devise_parameter_sanitizer.permit(:account_update,keys: [:post_code,:address,:self_introduction])
   end
 end
