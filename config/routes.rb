24bcb9df63169e449b726e001536f5c6819dd1a3
mononobe <ryoya.goemono@gmail.com>
2022-06-22 22:30:44 +0900

    メールをブラウザで確認できる

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
