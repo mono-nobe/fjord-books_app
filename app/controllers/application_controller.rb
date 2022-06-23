94bddf6854318b80aee77c42217a638e5da7597c
mononobe <ryoya.goemono@gmail.com>
2022-06-23 22:32:24 +0900

    ログアウト時にログインページへ遷移させる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters_to_sign_up, if: :devise_controller?
   before_action :configure_permitted_parameters_to_update_account, if: :devise_controller?
 
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
   def configure_permitted_parameters_to_sign_up
     devise_parameter_sanitizer.permit(:sign_up,keys: [:post_code,:address,:self_introduction])
   end
