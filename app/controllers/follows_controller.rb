c829648646b6fda194f3d2576ee34bf7d29e5646
mononobe <ryoya.goemono@gmail.com>
2022-07-03 21:26:35 +0900

    フォローする・解除する機能の実装

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,15 +1,15 @@
 class FollowsController < ApplicationController
   def create
-    puts "##### create #####"
-    puts params[:current_user]
+    followed_user = User.find(params[:followed_user_id])
+    current_user.follow(followed_user)
 
-    user = User.find(params[:current_user])
-    current_user.follow(user)
+    user_path(followed_user.id)
   end
 
   def destroy
-    user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user)
-    redirect_to user
+    followed_user = User.find(params[:id])
+    current_user.unfollow(followed_user)
+
+    user_path(followed_user.id)
   end
 end
