35707a5d7c74652445c168052dfbcd0370094120
mononobe <ryoya.goemono@gmail.com>
2022-07-03 22:03:51 +0900

    フォロー数とフォロワー数を表示するよう実装

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -3,13 +3,13 @@ class FollowsController < ApplicationController
     followed_user = User.find(params[:followed_user_id])
     current_user.follow(followed_user)
 
-    user_path(followed_user.id)
+    redirect_back fallback_location: user_path(followed_user.id)
   end
 
   def destroy
     followed_user = User.find(params[:id])
     current_user.unfollow(followed_user)
 
-    user_path(followed_user.id)
+    redirect_back fallback_location: user_path(followed_user.id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "FollowRelationship",
                             foreign_key: "follower_id",
                             dependent:   :destroy
-  has_many :passive_relationships, class_name:  "Relationship",
+  has_many :passive_relationships, class_name:  "FollowRelationship",
                                    foreign_key: "followed_id",
                                    dependent:   :destroy
   has_many :following, through: :active_relationships, source: :followed
