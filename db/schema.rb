fb39bdedea2a3b98d20932d158cf3d10b0564d87
mononobe <ryoya.goemono@gmail.com>
2022-08-21 02:03:14 +0900

    follower_idおよびfollowed_idカラムにNOT NULL制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_03_093539) do
+ActiveRecord::Schema.define(version: 2022_08_20_165829) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,8 +50,8 @@ ActiveRecord::Schema.define(version: 2022_07_03_093539) do
   end
 
   create_table "follow_relationships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["followed_id"], name: "index_follow_relationships_on_followed_id"
