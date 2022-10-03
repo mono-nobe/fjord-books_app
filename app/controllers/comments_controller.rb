b2b3e9b5f3632a0b957a0f34ce05097aec84be61
mononobe <ryoya.goemono@gmail.com>
2022-10-03 23:08:07 +0900

    不要なレスポンス(JSON)の削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,10 +7,8 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human) }
-        format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
