ee4a442b1acbfc553a6124b99ea5d2e188bf9a02
mono-nobe <ryoya.goemono@gmail.com>
2022-11-18 00:01:35 +0900

    Userモデルで投稿者名を返すメソッドを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,8 @@ class User < ApplicationRecord
     relationship = active_relationships.find_by(following_id: user.id)
     relationship&.destroy!
   end
+
+  def posted_by
+    name.presence || email
+  end
 end
